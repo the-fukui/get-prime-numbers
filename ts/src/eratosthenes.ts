1d663277799f26ca85246cd5b52ab1e7b8f14aba
the-fukui <account@the-fukui.com>
2022-08-04 01:26:20 +0900

    - 素数で有ることが確定した数を篩の対象から分離する - ループをforからArray.someに変更

--- ts/src/eratosthenes.ts
+++ ts/src/eratosthenes.ts
@@ -6,15 +6,22 @@ const getPrimeNumbers = (maximum: number) => {
   let list = [...Array(maximum)].map((_, i) => i + 1); // 1 ~ maximumまでの数字配列 [1,2,3,....,maximum]
   list.shift(); // 1を除外
 
-  for (let i = 0; i + 1 < list.length; i++) {
-    // 数字配列の長さ分ループ
-    if (!list[i]) break; // インデックスで数字配列にアクセスできなくなれば（ふるい落とされきったら）ループ終了
+  const primeNumbers: number[] = [];
 
-    // 現在の数字の倍数をふるい落としていく （ただし現在の数字そのものは残す）
-    list = list.filter((num) => num === list[i] || num % list[i] !== 0);
-  }
+  list.some((_, i) => {
+    if (!list[0]) return true;
 
-  return list;
+    // 0番目の数字は必ず素数なので素数配列に格納
+    primeNumbers.push(list[0]);
+
+    // 現在ループ中の数字の倍数をふるい落としていく
+    // 更新後のlistは、「現在ループ中の数字より大きい」 && 「素数の可能性がある数字」
+    list = list.filter((num) => num % list[0] !== 0);
+  });
+
+  return primeNumbers;
 };
 
+console.time('eratosthenes');
 console.log(getPrimeNumbers(50));
+console.timeEnd('eratosthenes');
