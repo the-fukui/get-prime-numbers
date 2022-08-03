e10e05941b6f3d33e18a5c2677866a74ddaf578a
the-fukui <account@the-fukui.com>
2022-08-04 01:45:42 +0900

    ループ処理をforに書き換え

--- ts/src/eratosthenes.ts
+++ ts/src/eratosthenes.ts
@@ -8,16 +8,20 @@ const getPrimeNumbers = (maximum: number) => {
 
   const primeNumbers: number[] = [];
 
-  list.some((_, i) => {
-    if (!list[0]) return true;
+  for (; true; ) {
+    if (!list[0]) break;
 
     // 0番目の数字は必ず素数なので素数配列に格納
     primeNumbers.push(list[0]);
 
     // 現在ループ中の数字の倍数をふるい落としていく
-    // 更新後のlistは、「現在ループ中の数字より大きい」 && 「素数の可能性がある数字」
-    list = list.filter((num) => num % list[0] !== 0);
-  });
+    // filteredlistは、「現在ループ中の数字より大きい」 && 「素数の可能性がある数字」
+    const filteredList: number[] = [];
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] % list[0] !== 0) filteredList.push(list[i]);
+    }
+    list = filteredList;
+  }
 
   return primeNumbers;
 };
